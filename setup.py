bcd98b5e1d1c6bc40c46945c93684021ce6c3ec3
Raymond Yee <raymond.yee@gmail.com>
2023-06-16 20:21:01 -0700

    setting -mmacosx-version-min to 10.13

--- setup.py
+++ setup.py
@@ -2,14 +2,20 @@ from setuptools import setup, Extension, find_packages
 
 setup(
     name="pasteboard",
-    version="0.3.3",
+    version="0.3.4",
     packages=find_packages("src"),
     package_dir={"": "src"},
     ext_modules=[
         Extension(
             "pasteboard._native",
             sources=["src/pasteboard/pasteboard.m"],
-            extra_compile_args=["-Wall", "-Wextra", "-Wpedantic", "-Werror"],
+            extra_compile_args=[
+                "-Wall",
+                "-Wextra",
+                "-Wpedantic",
+                "-Werror",
+                "-mmacosx-version-min=10.13",
+            ],
             extra_link_args=["-framework", "AppKit"],
             language="objc",
         )
